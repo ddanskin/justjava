7f3565a56f25aa5a7c6c02b926aa9f2c1d2fc041
Danielle Danskin <ddanskin@users.noreply.github.com>
2018-07-18 16:08:14 -0400

    add toppings header and "whipped cream" checkbox with label.

--- app/src/main/java/com/danielledanskin/justjava/MainActivity.java
+++ app/src/main/java/com/danielledanskin/justjava/MainActivity.java
@@ -19,40 +19,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /*
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
     // This method is called when the order button is clicked.
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total = $" + price;
-        priceMessage = priceMessage + "\n Thank you!";
-        displayMessage(priceMessage);
+        int price = calculatePrice();
+        String orderSummaryMessage = createOrderSummary(price);
+        displayMessage(orderSummaryMessage);
+    }
+
+    // This method creates the order summary as a string and returns it
+    private String createOrderSummary(int price) {
+        String orderSummary = "Name: Bob Belcher";
+        orderSummary += "\nQuantity: " + quantity;
+        orderSummary += "\nTotal = $" + price;
+        orderSummary += "\nThank you!";
+        return orderSummary;
     }
 
     // This method displays the given quantity value on the screen.
-    private void display(int number) {
+    private void displayQuantity(int count) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    //This method displays the price
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        quantityTextView.setText("" + count);
     }
 
     public void increment(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         if (quantity > 0) {
             quantity--;
         }
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
